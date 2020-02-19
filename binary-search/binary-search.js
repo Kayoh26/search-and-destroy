74786349a6b74618c7044fc9c45872ec735b98eb
Karen <k.ortiz026@gmail.com>
2020-02-19 15:43:10 -0500

    Finished binary-search

--- binary-search/binary-search.js
+++ binary-search/binary-search.js
@@ -2,7 +2,27 @@
 
 // Complete this algo
 const binarySearch = (array, target) => {
-	
+	if(array.length <= 1){
+		return array[0] === target ? true : false
+	} else {
+		const midpoint = Math.floor(array.length/2);
+		//console.log('midpoint', midpoint, 'target', target)
+		//console.log('array[midpoint] === target', array[midpoint] === target)
+		if(array[midpoint] === target){
+			return true
+		} else {
+			if (target > array[midpoint]){
+				const rightArray = array.slice(midpoint)
+				//console.log('right Array', rightArray)
+				return binarySearch(rightArray, target)
+			} else {
+				const leftArray = array.slice(0,midpoint)
+				//console.log('left Array', leftArray)
+				return binarySearch(leftArray, target)
+			}
+		}
+
+	}
 };
 
 /*
